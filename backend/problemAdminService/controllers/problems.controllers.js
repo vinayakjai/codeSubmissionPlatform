e0f4f82b6c605b13488e0425852ace9e41733402
vinayakjain <vinaaykjain388@gmail.com>
2024-03-20 13:42:18 +0530

    added routes corresponding to each problem controller

--- backend/problemAdminService/controllers/problems.controllers.js
+++ backend/problemAdminService/controllers/problems.controllers.js
@@ -5,6 +5,22 @@ function getProblems(req,res){
   
 }
 
+function getProblem(req,res){
+
+}
+
+function createProblem(req,res){
+
+}
+
+function updateProblem(req,res){
+
+}
+
+
 module.exports={
     getProblems,
+    getProblem,
+    createProblem,
+    updateProblem,
 }
